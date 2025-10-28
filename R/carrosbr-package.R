126510f5501d6632a3d97815d6032d33b9ee112a
Tomas de Siervi Barcellos <tomasbarcellos@gmail.com>
2025-10-27 23:23:05 -0300

    inclui documentação e resolve problemas de check()

---
+++ R/carrosbr-package.R
@@ -0,0 +1,10 @@
+#' @description
+#' Um pacote para ver dados de carros no Brasil.
+#' @keywords internal
+#' @import fipe
+#' @import fenabrave
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
